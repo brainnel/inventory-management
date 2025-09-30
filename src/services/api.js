ed5645a4f72bc71a5327e3b7be17fcf7760b0135
louyifei <louyifei@ventus.ai>
2025-09-30 14:38:34 +0800

    V1上线版本

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: 'https://api.brainnel.com/inventory/api/v1',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
